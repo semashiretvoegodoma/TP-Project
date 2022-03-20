6510956cb748e0460e09703fb1e77fa086a508af
Semyon <96422859+semashiretvoegodoma@users.noreply.github.com>
2022-03-20 16:56:39 +0300

    Update main.py

--- files/main.py
+++ files/main.py
@@ -1,20 +1,6 @@
 import pygame
 from currentScene import CurrentScene
 
-#отвечает за экран
-screen_width = 1300
-screen_height = 700
-screen = pygame.display.set_mode((screen_width, screen_height))
-
-#отвечает за название экрана
-pygame.display.set_caption("Arkanoid3000")
-
-#отвечает за иконку, она находится слева сверху
-#image = pygame.image.load("Arkanoid.png")
-#pygame.display.set_icon(image)
-
-# цвет текста
-text_color = (0, 0, 0)
 
 currentScene = CurrentScene()
 
@@ -22,14 +8,8 @@ currentScene = CurrentScene()
 # также за обновление экрана
 running = True
 while running:
-
     currentScene.update()
     currentScene.draw()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    pygame.display.update()
-
-pygame.quit()
+    cycle(running)
+quit()
