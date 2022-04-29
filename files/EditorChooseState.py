2b1814b2741c378ced1f2c8c3c2197502bd81968
stevegamer1 <kornei.sd@phystech.edu>
2022-04-29 22:03:51 +0300

    Editor initial scene ready.

--- files/EditorChooseState.py
+++ files/EditorChooseState.py
@@ -1,9 +1,37 @@
+from button import Button
+
+
 class EditorChooseState:
-    def __init__(self):
-        pass
+    def __init__(self, editorScene):
+        self.editorScene = editorScene
+        self.create_button = Button(500, 300, 140, 40, "Create new level!", "create")
+        self.create_button.addActionReceiver(self)
+        self.load_button = Button(500, 400, 140, 40, "Load level", "load")
+        self.load_button.addActionReceiver(self)
+        self.back_button = Button(500, 500, 140, 40, "Back", "back")
+        self.back_button.addActionReceiver(self)
 
     def update(self):
-        pass
+        self.create_button.update()
+        self.load_button.update()
+        self.back_button.update()
 
     def draw(self):
-        pass
+        self.create_button.draw()
+        self.load_button.draw()
+        self.back_button.draw()
+
+    def to_create(self):
+        self.editorScene.state = self.editorScene.STATE_CREATE
+
+    def to_load(self):
+        self.editorScene.state = self.editorScene.STATE_LOAD
+
+    def to_menu(self):
+        self.editorScene.to_menu()
+
+    def on_button(self, action):
+        if action == "create":
+            self.to_create()
+        elif action == "load":
+            self.to_load()
--- files/EditorScene.py
+++ files/EditorScene.py
@@ -5,13 +5,14 @@ from EditorNewLevelState import EditorNewLevelState
 
 
 class EditorScene:
-    def __init__(self):
+    def __init__(self, currentScene):
+        self.currentScene = currentScene
         self.STATE_CHOOSE_VARIANT = 0
         self.STATE_LOAD = 1
         self.STATE_CREATE = 2
         self.STATE_EDIT = 3
         self.state = self.STATE_CHOOSE_VARIANT
-        self.choose_state = EditorChooseState()
+        self.choose_state = EditorChooseState(self)
         self.load_state = EditorLoadLevelState()
         self.create_state = EditorNewLevelState()
         self.edit_state = EditorEditingState()
@@ -21,6 +22,9 @@ class EditorScene:
             self.STATE_CREATE: self.create_state,
             self.STATE_EDIT: self.edit_state
         }
+
+    def to_menu(self):
+        self.currentScene.state = self.currentScene.SCENE_MENU
 
     def update(self):
         self.state_dict[self.state].update()
