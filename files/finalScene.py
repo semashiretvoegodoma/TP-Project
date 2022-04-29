ab200437f627c5bb47d043207ae84eab78efa256
stevegamer1 <kornei.sd@phystech.edu>
2022-04-29 08:34:39 +0300

    Very nice final scene.

--- files/finalScene.py
+++ files/finalScene.py
@@ -18,8 +18,10 @@ class FinalWinScene(scene.Scene):
         red = int(128 + 128 * math.sin((time.time() - self.time_moment) * 1.6564))
         green = int(128 + 128 * math.sin((time.time() - self.time_moment) * 2.0566))
         blue = int(128 + 128 * math.sin((time.time() - self.time_moment) * 3.6563))
-        size = int(40 + 10 * math.sin(time.time() - self.time_moment))
-        wrapper.drawText("CREATORS:", (red, green, blue), size, "Arial", 300, 300)
+        size = int(50 + 40 * math.sin((time.time() - self.time_moment)*1.6))
+        x = int(300 + 300 * math.sin(time.time() - self.time_moment - 5))
+        y = int(300 + 200 * math.sin((time.time() - self.time_moment - 4)*1.1))
+        wrapper.drawText("CREATORS:", (red, green, blue), size, "Comic Sans", x, y)
         fonts = ["Comic Sans", "Courier", "Arial", "Verdana"]
         random.shuffle(fonts)
         wrapper.drawText(self.creators[0], (255, 0, 0), 35, fonts[0], 320, 360)
